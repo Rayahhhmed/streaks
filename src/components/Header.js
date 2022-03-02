1f2c41bf8cf52eaa858539ed6e612b617a05e2c5
Manhua Lu <z5364339@ad.unsw.edu.au>
2022-03-02 21:24:58 +1100

    Added the title for header and line

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
-import { Button } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+// import { Button } from '@mui/material';
+// import DeleteIcon from '@mui/icons-material/Delete';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { spacing } from '@mui/system';
+
 
 const Header = ({ title }) => {
   return (
-    <div>
-      <Button variant='outlined'>{title} <DeleteIcon /></Button>
-    </div>
+    <>
+    <Typography variant="h4" color="primary" id="title">
+      STREAKS
+    </Typography>
+    <ColoredLine color="black" />
+    </>
+    
   )
 }
+
+const ColoredLine = ( {color} ) => (
+  <hr
+    style={{
+      color: color,
+      backgroundColor: color,
+      height: 5
+    }}
+  />
+);
 
 export default Header
