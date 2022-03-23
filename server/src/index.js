abfa80b3a5de4bf68d775181686df4e2412e9cbb
jedwed <83894732+jedwed@users.noreply.github.com>
2022-03-23 21:15:55 +1100

    Backend can now read habits from a JSON file

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/habits", (req, res) => {});
+const initHabits = () => {
+  const data = fs.readFileSync("data.json");
+  return JSON.parse(data);
+};
+
+const storeHabits = () => {
+  fs.writeFileSync("data.json", JSON.stringify(habits));
+};
+
+app.get("/habits", (req, res) => {
+  res.json(habits);
+});
 
 app.get("/habits/:id", (req, res) => {});
 
@@ -14,6 +28,9 @@ app.post("/habits", (req, res) => {});
 app.put("/habits/:id", (req, res) => {});
 
 app.delete("/habits/:id", (req, res) => {});
+
+const habits = initHabits();
+console.log(habits);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
